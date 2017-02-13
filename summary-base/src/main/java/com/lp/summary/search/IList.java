167c7cf26520f7a5188ed3443e07dd29d2592f0f
lipei <lipei@Lenovo-PC>
2017-02-13 20:58:53 +0800

    线性表的抽象类型

---
+++ summary-base/src/main/java/com/lp/summary/search/IList.java
@@ -0,0 +1,66 @@
+package com.lp.summary.search;
+
+import org.apache.poi.ss.formula.functions.T;
+
+/**
+ * Created by Administrator on 2017/2/13.
+ * 线性表数据抽象类型
+ */
+public interface IList {
+    /**
+     * 是否为空
+     * @return
+     */
+    boolean isEmpty();
+
+    /**
+     * 表的长度
+     * @return
+     */
+    int length();
+
+    /**
+     * 根据索引获取长度
+     * @param i
+     * @return
+     */
+    T get(int i);
+
+    /**
+     * 设置第i个元素值为x
+     * @param i
+     * @param x
+     */
+    void set(int i,T x);
+
+    /**
+     * 在线性表最后插入x元素
+     * @param x
+     */
+    void append(T x);
+
+    /**
+     * 异常第i个元素并返回值
+     * @param i
+     * @return
+     */
+    T remove(int i);
+
+    /**
+     * 删除线性表中所有元素
+     */
+    void removeAll();
+
+    /**
+     * 查询首次出现关键字为key的元素
+     * @param key
+     * @return
+     */
+    T search(T key);
+
+
+
+
+
+
+}
