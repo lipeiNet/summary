24e07e30f29ee7368dcb98b98e4fe542fb270c9d
lipei <lipei@Lenovo-PC>
2017-02-14 17:47:17 +0800

    线性接口

--- summary-base/src/main/java/com/lp/summary/lists/IList.java
+++ summary-base/src/main/java/com/lp/summary/lists/IList.java
@@ -1,16 +1,60 @@
 package com.lp.summary.lists;
 
 /**
- * Created by Administrator on 2016/12/8.
+ * Created by Administrator on 2017/2/13.
+ * 线性表数据抽象类型
  */
 public interface IList<T> {
-     boolean isEmpty();
-     int length();
-     T get(int i);
-     void set(int i, T x);
-     void append(T x);
-     T remove(int i);
-     void removeAll();
-     T search(T key);
-     void insert(int i,T x);
+    /**
+     * 是否为空
+     * @return
+     */
+    boolean isEmpty();
+
+    /**
+     * 表的长度
+     * @return
+     */
+    int length();
+
+    /**
+     * 根据索引获取长度
+     * @param i
+     * @return
+     */
+    T get(int i);
+
+    /**
+     * 设置第i个元素值为x
+     * @param i
+     * @param x
+     */
+    void set(int i,T x);
+
+    /**
+     * 在线性表最后插入x元素
+     * @param x
+     */
+    void append(T x);
+
+    /**
+     * 异常第i个元素并返回值
+     * @param i
+     * @return
+     */
+    T remove(int i);
+
+    /**
+     * 删除线性表中所有元素
+     */
+    void removeAll();
+
+    /**
+     * 查询首次出现关键字为key的元素
+     * @param key
+     * @return
+     */
+    T search(T key);
+
+    void insert(int i,T x);
 }
