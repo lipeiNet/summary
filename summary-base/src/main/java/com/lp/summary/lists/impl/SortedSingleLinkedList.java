bda1d9c108e27bd0f87c9318aa30c080b8f2c285
lipei <lipei@Lenovo-PC>
2017-02-15 17:00:09 +0800

    升序的单链表

---
+++ summary-base/src/main/java/com/lp/summary/lists/impl/SortedSingleLinkedList.java
@@ -0,0 +1,42 @@
+package com.lp.summary.lists.impl;
+
+import com.lp.summary.lists.Node;
+
+/**
+ * Created by Administrator on 2017/2/15.
+ */
+public class SortedSingleLinkedList<T extends Comparable<T>> extends SingleLinkedList<T> {
+
+    public void insert(T x) {
+        if (x == null) {
+            return;
+        }
+        Node<T> p = this.head;
+        Node<T> prev = p.next;
+        while (p != null && p.data.compareTo(x) < 0) {
+            prev = p;
+            p = p.next;
+        }
+        prev.next = new Node<T>(x, p);
+    }
+    public void insert(int i,T x){
+        throw new UnsupportedOperationException("insert(int i,T x)");
+    }
+    public void append(T x) {
+        throw new UnsupportedOperationException("append(T x)");
+    }
+    public void remove(T x){
+        if (x==null){
+            return;
+        }
+        Node<T> p = this.head;
+        Node<T> prev = p.next;
+        while (p!=null&&p.data.compareTo(x)<0){
+            prev=p;
+            p=p.next;
+        }
+        if (p.data.compareTo(x)==0){
+            prev.next=p.next;
+        }
+    }
+}
