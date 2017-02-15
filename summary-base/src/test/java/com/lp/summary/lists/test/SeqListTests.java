e23b5922c844fd3e850eb4305e80916f71c1fb15
lipei <lipei@Lenovo-PC>
2017-02-15 17:28:21 +0800

    顺序表测试

---
+++ summary-base/src/test/java/com/lp/summary/lists/test/SeqListTests.java
@@ -0,0 +1,39 @@
+package com.lp.summary.lists.test;
+
+import com.lp.summary.lists.IList;
+import com.lp.summary.lists.impl.SeqList;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Administrator on 2017/2/14.
+ */
+@RunWith(JUnit4.class)
+public class SeqListTests {
+    @Test
+    public void isEmpty() {
+        IList<String> list = new SeqList<String>();
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void length() {
+        IList<String> list = new SeqList<String>();
+        list.append("1");
+        assertEquals(1, list.length());
+    }
+    @Test
+    public void insert(){
+        IList<String> list = new SeqList<String>();
+        list.append("1");
+        list.append("2");
+        list.insert(1,"3");
+        assertEquals("3",list.get(1));
+    }
+}
